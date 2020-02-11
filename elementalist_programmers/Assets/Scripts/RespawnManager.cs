56075acb01d0f753dbe4002bebd98db67fb7e274
Austin Meyer <meyerforge@gmail.com>
2020-02-11 15:12:08 -0800

    Add RespawnManager.cs to manage the respawn object

---
+++ elementalist_programmers/Assets/Scripts/RespawnManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag.Equals("Player"))
+        {
+            col.GetComponent<PlayerController>().SetRespawnPoint(this.gameObject);
+        }
+    }
+}
