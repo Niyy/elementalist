65e14a55b4661eac001ceb5654053165569a24d4
Olah98 <aolah@mail.csuchico.edu>
2020-02-10 18:08:35 -0800

    C teir enemy has been done for a few days just forgot to push

---
+++ elementalist_programmers/Assets/Scripts/EnemyC.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyC : Enemy
+{
+
+    
+    private Rigidbody rb;
+    private Vector2 movement;
+    
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        WhoAmIKilling();
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.transform.position - transform.position;
+        
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate()
+    {
+        moveChar(movement);
+    }
+
+    void moveChar(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+
+
+
+}
