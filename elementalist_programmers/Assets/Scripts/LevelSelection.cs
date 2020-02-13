653d30452abf70eff91afacf2383716aea888c14
Olah98 <aolah@mail.csuchico.edu>
2020-02-13 15:53:31 -0800

    Level Selection screne

---
+++ elementalist_programmers/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    public string myLevel;
+
+   public void goToThisLevel()
+    {
+        SceneManager.LoadScene(myLevel);
+    }
+}
