3b1f4e65bd41ad79ba5e2c9f85fff94dbd54e4d6
Austin Meyer <meyerforge@gmail.com>
2020-02-17 15:01:06 -0800

    Basic patrolling has been implemented.

--- elementalist_programmers/Assets/Scripts/EnemyB.cs
+++ elementalist_programmers/Assets/Scripts/EnemyB.cs
@@ -8,11 +8,14 @@ public class EnemyB : Enemy
     [Range(-1, 1)]
     public int direction;
     public float patrol_wait_timer;
+    public float search_arch;
+    public float sight_distance;
+
 
     private float current_patrol_timer;
 
 
-    private Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
     private float lowJumpMultiplier = 2f;
 
 
@@ -23,6 +26,7 @@ public class EnemyB : Enemy
         rigidbody = this.GetComponent<Rigidbody>();
     }
 
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -30,20 +34,47 @@ public class EnemyB : Enemy
     }
 
 
+    private void Update()
+    {
+        DebugStatements();
+    }
+
+
     private void EngageMovement()
     {
         Vector2 next_position = this.transform.position + new Vector3(direction * 0.75f, 0.0f, 0.0f);
         RaycastHit check_down;
-
-        if (!Physics.Raycast(next_position, Vector2.down * 2.0f, out check_down, 1.0f))
+        if(current_patrol_timer > patrol_wait_timer)
         {
-            direction = -direction;
+            current_patrol_timer += Time.deltaTime;
+            this.rigidbody.velocity = Vector2.zero;
         }
         else
         {
-            this.rigidbody.velocity = new Vector2(speed * direction, this.rigidbody.velocity.y);
+            if (!Physics.Raycast(next_position, Vector2.down * 2.0f, out check_down, 1.0f))
+            {
+                direction = -direction;
+                current_patrol_timer = 0;
+            }
+            else
+            {
+                this.rigidbody.velocity = new Vector2(speed * direction, this.rigidbody.velocity.y);
+            }
         }
 
         Debug.DrawRay(next_position, Vector2.down, Color.green);
+    }
+
+
+    private void SearchLineOfSight()
+    {
+        RaycastHit line_of_sight;
+
+    }
+
+
+    private void DebugStatements()
+    {
+        Debug.DrawRay(this.transform.position + (Vector2.right * offset), new Vector2(direction, 0.0f), Color.yellow, sight_distance);
     }
 }
