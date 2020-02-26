47bb6e4e91c1acb0109a1051d10ad2ee6bd016e2
Olah98 <aolah@mail.csuchico.edu>
2020-02-25 16:25:31 -0800

    New room movment system in place

--- elementalist_programmers/Assets/Scripts/LevelManager.cs
+++ elementalist_programmers/Assets/Scripts/LevelManager.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    static public LevelManager _instance;
+    static public LevelManager Instance { get { return _instance; } }
+
     public int coins = 0;
+    
 
-    public int coinsNeededForBoss;
+    public GameObject[] roomsCenter;
 
-    public NextLevel bossDoor;
+    
+   [HideInInspector] public List<Vector3> roomsPosInLevel;
 
-    public List<GameObject> roomsInLevel;
+    public GameObject camTarget;
 
-    private void Update()
-    {
-        //if (coins >= coinsNeededForBoss)
-           // bossDoor.closedDoor = false;
+    private void Awake()
+    {//singleton
+        if(_instance !=null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+        //takes center of each level and adds the transform to a list
+        foreach(GameObject room in roomsCenter)
+        {
+            roomsPosInLevel.Add(room.transform.position);
+        }
+        //finds what the cam is looksing at
+        camTarget = GameObject.Find("CamTarget");
+
+        camTarget.transform.position = roomsPosInLevel[0];
     }
+
+
+
+
 
 
     public void AddToCollectedCoins(int amount_add = 1)
