a65d7e1fdcc594c6e733f66cc5fb036f211f8a7c
Austin Meyer <meyerforge@gmail.com>
2020-03-05 10:11:25 -0800

    Found and crushed secondary PlayerController.cs.

---
+++ elementalist_programmers/Assets/Scripts/Levels/BossDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BossDoor : MonoBehaviour
+{
+    public bool open = false;
+    bool opened = false;
+    private void Update()
+    {
+        if(opened)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                print("i qiut");
+                Application.Quit();
+                
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(open)
+            {
+                print("you WIN ");
+                LevelManager._instance.winState();
+                opened = true;
+              
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+
+}
