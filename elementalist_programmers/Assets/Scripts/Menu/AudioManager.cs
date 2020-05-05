c5df1cc2aeb796a2ae35591196b7dd416995f96d
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2020-05-05 04:29:59 -0700

    volume settings

---
+++ elementalist_programmers/Assets/Scripts/Menu/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+    public AudioMixer sfxMixer;
+    public float musicVol = 1;
+    public float sfxVol = 1;
+
+
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void SetMusicVolume(float slider)
+    {
+        musicVol = slider;
+
+        musicMixer.SetFloat("MusicVol", Mathf.Log10(slider) * 20);
+    }
+
+    public void SetSfxVolume(float slider)
+    {
+        sfxVol = slider;
+        sfxMixer.SetFloat("SfxVol", Mathf.Log10(slider) * 20);
+    }
+}
---
+++ elementalist_programmers/Assets/Scripts/Menu/SetVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public Slider MusicSlider;
+    public Slider SfxSlider;
+
+    void Start()
+    {
+        MusicSlider.value = AudioManager.Instance.musicVol;
+        SfxSlider.value = AudioManager.Instance.sfxVol;
+    }
+}
